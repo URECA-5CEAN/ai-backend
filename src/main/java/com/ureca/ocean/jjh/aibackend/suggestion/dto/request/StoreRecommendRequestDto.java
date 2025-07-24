9d5973e759efd76e0b472f3fe366a1c27cb4dae2
Jisan <kjs25481@gmail.com>
2025-07-24 14:14:28 +0900

    feat : StoreRecommendRequestDto 값 수정

--- src/main/java/com/ureca/ocean/jjh/aibackend/suggestion/dto/request/StoreRecommendRequestDto.java
+++ src/main/java/com/ureca/ocean/jjh/aibackend/suggestion/dto/request/StoreRecommendRequestDto.java
@@ -1,14 +1,21 @@
 package com.ureca.ocean.jjh.aibackend.suggestion.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoreRecommendRequestDto {
-	private String latitude;
-	private String longitude;
-	private int redius;
+	private String keyword;
+    private String category;
+    private Double latMin;
+    private Double latMax;
+    private Double lngMin;
+    private Double lngMax;
+    private Double centerLat;
+    private Double centerLng;
 }
